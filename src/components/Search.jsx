abb2e21e7b234043780be6c4aae4525291d81697
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-05-13 20:07:09 +0300

    deliverables met

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,32 @@
-export default function Search() {
+
+
+import React, { useEffect } from 'react';
+import UserCard from './UserCard';
+
+export default function Search({users,setRandomUser}) {
+
+
+
+const getRandomUser =()=>{
+    const randomIndex= Math.floor(Math.random()*users.length)
+    console.log(randomIndex)
+    setRandomUser(users[randomIndex])
+    console.log(users[randomIndex])
+    // setShowRandomUser(true)
+    
+
+}
+
+useEffect(()=>{
+    if (users.length > 0) {
+        getRandomUser();
+    }
+},['https://jsonplaceholder.typicode.com/users/'])
+
     return (
         <div className="search">
-            <input className="search-number" type="button" value="Get Random User"/>
+            <input className="search-number" type="button" value="Get Random User" onClick={getRandomUser}/>
+            
         </div>
     )
 }
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,13 +1,25 @@
 import UserCard from './UserCard'
+import React, {  useState } from 'react';
 import Search from './Search'
 
 export default function UserList({users}) {
+    const [randomUser,setRandomUser]=useState(null);
+    // const [showRandomUser,setShowRandomUser]=useState(false);
 
     return (
         <>
-            <Search />
+            <Search 
+            users={users}
+            setRandomUser={setRandomUser}
+            // setShowRandomUser={setShowRandomUser}
+            
+            />
             <div className="user-list">
-                {users.map((user) => UserCard(user))}
+                 {randomUser ? (
+          <UserCard {...randomUser} />
+        ) : (
+          users.map((user) => <UserCard key={user.id} {...user} />)
+        )}
             </div>
         </>
     )
